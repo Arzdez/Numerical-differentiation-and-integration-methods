62b9f01a00f7914d59947d43e9340b05964537a3
Arzdez <72273943+Arzdez@users.noreply.github.com>
2021-03-17 15:26:57 +0400

    Доработал

--- 1.py
+++ 1.py
@@ -1,23 +1,25 @@
 import difmetods as dif
 import numpy as np
 import matplotlib.pyplot as plt
-sins = []
-cos = []
-msins = []
-T1 = []
+################################################################
+#генерирование данных
+sins = [np.sin(i) for i in np.arange(-5,5,0.01)]
+cos = [np.cos(i) for i in np.arange(-5,5,0.01)]
+msins = [-np.sin(i) for i in np.arange(-5,5,0.01)]
+T1 = [i for i in np.arange(-5,5,0.01)]
+#################################################################
+#Константы
 m1 = 5
 m21 = int((m1-1)/2)
-for i in np.arange(-5,5,0.01):
-    sins.append(np.sin(i))
-    cos.append(np.cos(i))
-    msins.append(-np.sin(i))
-    T1.append(i)
+AorB = 1
+##################################################################
+
 dsin = dif.parabdif2(sins,T1,m1,2)
-d2sin = dif.parabdif2(dsin,T1[m21:len(T1)-m21],m1,2)
+d2sin = dif.parabdif2(sins,T1,m1,AorB)
 
 plt.plot(T1[m21:len(T1)-m21], dsin,color = 'red')
 plt.plot(T1,cos,"--",color = 'green')
 plt.plot(T1,sins)
-plt.plot(T1[2*m21:len(T1)-2*m21],d2sin,color = 'blue')
+plt.plot(T1[m21:len(T1)-m21],d2sin,color = 'blue')
 plt.plot(T1,msins,'--')
 plt.show()
