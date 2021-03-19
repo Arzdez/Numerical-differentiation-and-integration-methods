2182025b51b6f16d9ab4b6ca8e7b73b166201dfa
Arzdez <72273943+Arzdez@users.noreply.github.com>
2021-03-19 16:04:21 +0400

    правка симсона

--- integrmetods.py
+++ integrmetods.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
+startTime = time.time()
 #sins = [ np.sin(i) for i in np.arange(0,np.pi/2,1)]
 #x = [i for i in np.arange(0,np.pi/2,1)]
 
@@ -8,20 +10,18 @@ def sins(x):
     return np.sin(x)
 a = 0
 b = np.pi/2
-
 # правые прямоугольники
-def right(a,b,n):
-    sumy = 0
+def Iright(Fx,a,b,n):
+    #sumy = 0
     dx=(b-a)/n
     result = 0
     for i in range(n):
-        sumy+=dx
-        result += sins(sumy)
+        result += Fx(dx*i)
 
     return(result*dx)
 
-def midle(a,b,n):
-    dx = (a-b)/n
+def Imidle(Fx,a,b,n):
+    dx = (b-a)/n
     xd = dx/2
     result = 0
     sumy = 0
@@ -29,44 +29,37 @@ def midle(a,b,n):
     for i in range(n):
         sumy += dx
         sumy2 = sumy - xd
-        result += sins(sumy2)
+        result += Fx(sumy2)
     return result*dx
 
-def trapeciya(a,b,n):
-    dx = (a-b)/n
-    result = 0
+def Itrapeciya(Fx,a,b,n):
+    dx = (b-a)/n
     sumy = 0
-    xd = 0
     for i in range(n):
-        xd+=dx
-        sumy+=sins(xd)
-    
-    I = dx*( ((sins(a)+sins(b))/2)+sumy)
+        sumy+=Fx(dx*i)
+    I = dx*( ((Fx(a)+Fx(b))/2)+sumy)
     return( I)
 
-def simpson(a,b,n):
-    dx = (a-b)/n
-    xd = dx/2
-    sumy = 0
-    sumy2 = 0
-    suma = 0
-    for i in range(1,n*2-1):
-        sumy += dx
-        sumy2 = sumy - xd
-        suma +=sins(sumy2)
+def Isimpson(Fx,a,b,n): 
+    h=((b-a)/n)
+    s=Fx(a)+Fx(b)
+    for i in range(1,n):
+        if i%2==0:
+            s+= 2*Fx(a+h*i) 
+        else:
+            s+= 4*Fx(a+h*i)
+    return(s*h/3)
+
+#def ImonteKarlo(Fx,a,b,n):
+    #p1
     
-    #print(len(T))
-    #sum1 = 4*sum([sins(T[i]) for i in np.arange(1,n*2+1,1)])
-    #sum2 = 2*sum([sins(T[i]) for i in np.arange(1,n*2,1)])
 
-    #print(sum1)
-    #print(sum2)
-    #I = dx/3 * ( sins(a)+sins(b)+sum1+sum2)
-   # return I
+print(Iright(sins,a,b,1000))
+print(Imidle(sins,a,b,1000))
+print(Itrapeciya(sins,a,b,1000))
+print(Isimpson(sins,a,b,1000))
 
-simpson(a,b,100)
 
-#print(right(a,b,100))
-#print(midle(a,b,100))
-#print(trapeciya(a,b,100))
-#clprint(simpson(a,b,100))
+endTime = time.time()
+totalTime = endTime - startTime
+print("Время = ", totalTime)
